7fb9b126eb6c8db5c7cdd155214196699acd627e
neline388 <negarlaleh1@gmail.com>
2025-12-05 11:43:14 +0100

    pictures fixed

--- src/app/middle-part.ts
+++ src/app/middle-part.ts
@@ -189,11 +189,11 @@ styles:[`
 })
 export class midcomponent {
 
-    imageSrc = 'assets/pictures/Gemini_Generated_Image_viqchtviqchtviqc(1).png';
+    imageSrc = './assets/pictures/Gemini_Generated_Image_viqchtviqchtviqc(1).png';
 
     swapImage(isHover: boolean) {
         this.imageSrc = isHover
-            ? 'assets/pictures/transparent_output(1).png'
-            : 'assets/pictures/Gemini_Generated_Image_viqchtviqchtviqc.png';
+            ? './assets/pictures/transparent_output(1).png'
+            : './assets/pictures/Gemini_Generated_Image_viqchtviqchtviqc.png';
     }
 }
